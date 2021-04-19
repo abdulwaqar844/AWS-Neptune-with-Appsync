da266c0857682ee776f50e77056502968f563ec6
Waqar <abdulwaqar844@gmail.com>
2021-04-19 15:34:37 +0500

    lambda updated

--- lambda-fns/createPost.ts
+++ lambda-fns/createPost.ts
@@ -1,20 +1,112 @@
-const gremlin = require('gremlin')
 import Post from './Post'
-
-const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection
-const Graph = gremlin.structure.Graph
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import {driver, process as gprocess, structure} from 'gremlin';
+import * as async from 'async';
 declare var process : {
     env: {
       NEPTUNE_ENDPOINT: string
     }
   }
-async function createPost(post: Post) {
-    let dc = new DriverRemoteConnection(`wss://${process.env.NEPTUNE_ENDPOINT}/gremlin`, {})
-    const graph = new Graph()
-    const g = graph.traversal().withRemote(dc)
+let conn: driver.DriverRemoteConnection;
+let g: gprocess.GraphTraversalSource;
 
-    await g.addV('posts').property('title',post.title).property('content', post.content).next()
-    dc.close()
-    return post
+async function query(post) {
+    return g.addV('posts').property('title',post.title).property('content', post.content).next();
 }
-export default createPost
+
+async function doQuery(post) {
+    let result = await query(post); 
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "text/plain" },
+        body: post,
+      };
+
+}
+export async function createPost(post: Post) {
+    const getConnectionDetails = () => {
+        const database_url = 'wss://' + process.env.NEPTUNE_ENDPOINT + ':8182/gremlin';
+        return { url: database_url, headers: {}};
+    };
+    const createRemoteConnection = () => {
+        const { url, headers } = getConnectionDetails();
+        return new driver.DriverRemoteConnection(
+            url, 
+            { 
+                mimeType: 'application/vnd.gremlin-v2.0+json', 
+                pingEnabled: false,
+                headers: headers 
+            });       
+    };
+    const createGraphTraversalSource = (conn: driver.DriverRemoteConnection) => {
+        return gprocess.traversal().withRemote(conn);
+    };
+    if (conn == null){
+        conn = createRemoteConnection();
+        g = createGraphTraversalSource(conn);
+    }
+
+
+
+
+return async.retry(
+    { 
+        times: 5,
+        interval: 1000,
+        errorFilter: function (err) { 
+            
+            // Add filters here to determine whether error can be retried
+            console.warn('Determining whether retriable error: ' + err.message);
+            
+            // Check for connection issues
+            if (err.message.startsWith('WebSocket is not open')){
+                console.warn('Reopening connection');
+                conn.close();
+                conn = createRemoteConnection();
+                g = createGraphTraversalSource(conn);
+                return true;
+            }
+            
+            // Check for ConcurrentModificationException
+            if (err.message.includes('ConcurrentModificationException')){
+                console.warn('Retrying query because of ConcurrentModificationException');
+                return true;
+            }
+            
+            // Check for ReadOnlyViolationException
+            if (err.message.includes('ReadOnlyViolationException')){
+                console.warn('Retrying query because of ReadOnlyViolationException');
+                return true;
+            }
+            
+            return false; 
+        }
+        
+    }, 
+   
+    doQuery(post)
+    )
+    ;
+
+   
+    
+}
+// const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection
+// const Graph = gremlin.structure.Graph
+// declare var process : {
+//     env: {
+//       NEPTUNE_ENDPOINT: string
+//     }
+//   }
+// async function createPost( {
+//     let dc = new DriverRemoteConnection
+//     (`wss:// + ${process.env.NEPTUNE_ENDPOINT} +:8182/gremlin`,
+//      {})
+//     const graph = new Graph()
+//     const g = graph.traversal().withRemote(dc)
+
+//     await 
+//     dc.close()
+//     return post
+// }
+// export default createPost
